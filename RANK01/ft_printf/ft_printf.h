e45982a313b69f8e80929b7cb11ac9f855bc7a8e
David Monteiro <102864199+davidmonteiro03@users.noreply.github.com>
2023-10-30 12:15:26 +0000

    something changed

---
+++ RANK01/ft_printf/ft_printf.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/06 18:17:55 by dcaetano          #+#    #+#             */
+/*   Updated: 2023/10/07 15:38:36 by dcaetano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include "libft/libft.h"
+# include <stdarg.h>
+
+int	ft_printf(const char *str, ...);
+int	ft_format(va_list args, const char format);
+int	ft_format_bonus(va_list args, const char c, const char format);
+
+#endif
