ae3c1684ab89557d9bad19308ec4bd20666bfca5
David Caetano Monteiro <dcaetano@c1r9s10.42porto.com>
2023-10-31 19:35:21 +0000

    something changed (let's introduce minilibx!)

--- RANK02/so_long/main.c
+++ RANK02/so_long/main.c
@@ -6,17 +6,32 @@
 /*   By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 12:49:32 by dcaetano          #+#    #+#             */
-/*   Updated: 2023/10/31 19:05:07 by dcaetano         ###   ########.fr       */
+/*   Updated: 2023/10/31 19:35:18 by dcaetano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/so_long.h"
 
-void	start_game(void)
+typedef struct	s_vars
 {
 	void	*mlx;
+	void	*win;
+}t_vars;
 
-	mlx = mlx_init();
+int	key_hook(int keycode)
+{
+	ft_printf("Key pressed: %d => %c\n", keycode, (char)keycode);
+	return (keycode);
+}
+
+void	start_game(void)
+{
+	t_vars	vars;
+
+	vars.mlx = mlx_init();
+	vars.win = mlx_new_window(vars.mlx, 600, 300, "so_long");
+	mlx_key_hook(vars.win, key_hook, &vars);
+	mlx_loop(vars.mlx);
 }
 
 int	main(int ac, char **av)
