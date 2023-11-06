98b3de70e7a19c704c4910f320415d9a3443edb6
David Caetano Monteiro <dcaetano@c1r9s10.42porto.com>
2023-11-06 18:14:13 +0000

    something changed

---
+++ RANK03/philo/includes/philo.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/06 17:44:08 by dcaetano          #+#    #+#             */
+/*   Updated: 2023/11/06 18:12:32 by dcaetano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <unistd.h>
+# include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+void	philo_putchar(char c);
+
+#endif
