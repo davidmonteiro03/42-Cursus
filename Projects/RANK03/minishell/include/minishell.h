e8fb24c4043bcb2050b1327704d851e440963721
David Caetano Monteiro <dcaetano@c1r9s10.42porto.com>
2023-11-13 10:45:16 +0000

    minishell introduced

---
+++ Projects/RANK03/minishell/include/minishell.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/27 16:39:23 by dcaetano          #+#    #+#             */
+/*   Updated: 2023/11/13 10:40:52 by dcaetano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "../libs/libft/libft.h"
+# include "../libs/libft/get_next_line.h"
+# include <readline/readline.h>
+# include <readline/history.h>
+
+void	teste(void);
+
+#endif
