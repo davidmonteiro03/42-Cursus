ab912511cc4cf7131e9067b872edcfa7846adb4e
davidmonteiro03 <damonteiro12446@gmail.com>
2023-11-20 22:26:14 +0000

    a little thing about quotes (minishell)...

--- my-lib/main.c
+++ my-lib/main.c
@@ -6,14 +6,14 @@
 /*   By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 19:17:21 by dcaetano          #+#    #+#             */
-/*   Updated: 2023/11/20 19:39:30 by dcaetano         ###   ########.fr       */
+/*   Updated: 2023/11/20 22:23:46 by dcaetano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "my_lib.h"
 #include <readline/readline.h>
 
-void	parse_quotes(char *line)
+void	count_chars(char *line, int *count)
 {
 	int		i;
 	int		status;
@@ -22,7 +22,6 @@ void	parse_quotes(char *line)
 	i = -1;
 	status = 0;
 	c = 0;
-	line = ft_strtrim(line, " ");
 	while (line[++i])
 	{
 		if (status == 0 && (line[i] == '\'' || line[i] == '\"'))
@@ -37,20 +36,56 @@ void	parse_quotes(char *line)
 			c = 0;
 			continue ;
 		}
-		if (line[i - 1] == ' ' && line[i] != ' ' && status == 0)
-			printf("\n");
-		printf("%c", line[i]);
-		if (line[i + 1] == c)
-			printf("\n");
+		if ((line[i] == ' ' && line[i - 1] != ' ' && status == 0) || \
+			((line[i] != ' ' && status == 0) || status == 1))
+			*count += 1;
 	}
-	printf("\n");
+}
+
+void	build_str(char *line, int j, char *buf)
+{
+	int		i;
+	int		status;
+	char	c;
+
+	i = -1;
+	status = 0;
+	c = 0;
+	while (line[++i])
+	{
+		if (status == 0 && (line[i] == '\'' || line[i] == '\"'))
+		{
+			status = 1;
+			c = line[i];
+			continue ;
+		}
+		if (status == 1 && line[i] == c)
+		{
+			status = 0;
+			c = 0;
+			continue ;
+		}
+		if ((line[i] == ' ' && line[i - 1] != ' ' && status == 0) || \
+			((line[i] != ' ' && status == 0) || status == 1))
+			buf[j++] = line[i];
+	}
 }
 
 int	main(void)
 {
 	char	*line;
+	char	*buf;
+	int		count;
 
 	line = readline("minishell$ ");
-	parse_quotes(line);
+	line = buildfree(line, " ", &ft_strtrim);
+	count = 0;
+	count_chars(line, &count);
+	buf = malloc(sizeof(char *) * (count + 1));
+	build_str(line, 0, buf);
+	free(line);
+	buf[count] = '\0';
+	printf("%s\n", buf);
+	free(buf);
 	return (0);
 }
