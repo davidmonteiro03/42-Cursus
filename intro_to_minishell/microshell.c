f6ba39b2cdb45404c4475d8fdc402ff4f5ae98b9
David Caetano Monteiro <dcaetano@c1r9s10.42porto.com>
2023-11-20 08:47:16 +0000

    a

--- intro_to_minishell/microshell.c
+++ intro_to_minishell/microshell.c
@@ -6,7 +6,7 @@
 /*   By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 08:34:08 by dcaetano          #+#    #+#             */
-/*   Updated: 2023/11/20 08:34:57 by dcaetano         ###   ########.fr       */
+/*   Updated: 2023/11/20 08:45:51 by dcaetano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,13 @@
 
 int	main(int ac, char **av)
 {
+	int		i;
+
+	if (ac < 2)
+		return (0);
 	i = 0;
+	while (av[++i])
+		printf("%s\n", av[i]);
+	j = 0;
 	return (0);
 }
