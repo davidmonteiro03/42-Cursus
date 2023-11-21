deee4917b0fbf32f584452ca5bd8db2e927f4abd
David Caetano Monteiro <dcaetano@c1r9s10.42porto.com>
2023-11-21 16:32:26 +0000

    the closest I can get is this... (minishell quotes)

--- my-lib/main.c
+++ my-lib/main.c
@@ -6,11 +6,12 @@
 /*   By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 19:17:21 by dcaetano          #+#    #+#             */
-/*   Updated: 2023/11/21 14:33:19 by dcaetano         ###   ########.fr       */
+/*   Updated: 2023/11/21 16:32:24 by dcaetano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "my_lib.h"
+#include <sys/wait.h>
 #include <readline/readline.h>
 
 typedef struct s_quotes
@@ -20,6 +21,7 @@ typedef struct s_quotes
 	char	*buf;
 	char	*line;
 	char	**args;
+	char	*cmd;
 }t_quotes;
 
 void	count_chars(t_quotes **q, int status, char c)
@@ -41,8 +43,6 @@ void	count_chars(t_quotes **q, int status, char c)
 			c = 0;
 			continue ;
 		}
-		if (i > 0 && ((*q)->line[i - 1] == c || (*q)->line[i + 1] == c))
-			(*q)->count++;
 		if (((*q)->line[i] == ' ' && (*q)->line[i - 1] != ' ' \
 			&& status == 0) || (((*q)->line[i] != ' ' && status == 0) \
 			|| status == 1))
@@ -78,60 +78,53 @@ void	build_str(int j, t_quotes **q, char c, int status)
 			c = 0;
 			continue ;
 		}
-		if (i > 0 && (*q)->line[i - 1] == c)
-			(*q)->buf[j++] = c;
 		if (((*q)->line[i] == ' ' && (*q)->line[i - 1] != ' ' && \
 			status == 0) || (((*q)->line[i] != ' ' && status == 0) \
 			|| status == 1))
 			(*q)->buf[j++] = get_char((*q)->line, status, i);
-		if ((*q)->line[i + 1] == c)
-			(*q)->buf[j++] = c;
 	}
 }
 
-void	check_arg(char *arg, int status, char c)
+void	analyse(t_quotes **q, char **env)
 {
-	int		i;
-
-	i = -1;
-	while (arg[++i])
-	{
-		if (status == 0 && (arg[i] == '\'' || arg[i] == '\"'))
-		{
-			status = 1;
-			c = arg[i];
-			continue ;
-		}
-		if (status == 1 && arg[i] == c)
-		{
-			status = 0;
-			c = 0;
-			continue ;
-		}
-		printf("%c", arg[i]);
-	}
-}
-
-void	analyse(t_quotes **q)
-{
-	int		i;
+	int		size;
+	char	**args;
 
 	(*q)->args = ft_split((*q)->buf, '\n');
-	i = -1;
-	while ((*q)->args[++i])
+	size = 0;
+	while ((*q)->args[size])
+		size++;
+	args = (char **)malloc(sizeof(char *) * (size + 1));
+	args[0] = "/bin/echo";
+	size = 1;
+	while ((*q)->args[size])
 	{
-		check_arg((*q)->args[i], 0, 0);
-		if ((*q)->args[i + 1])
-			printf(" +\n");
+		if ((*q)->args[size][0] == '$')
+		{
+			args[size] = ft_strdup(((*q)->args[size]));
+			args[size] = buildfree(args[size], "$", &ft_strtrim);
+			args[size] = getenv(args[size]);
+		}
+		else
+			args[size] = (*q)->args[size];
+		size++;
 	}
-	printf("\n");
+	args[size] = NULL;
+	(*q)->cmd = args[0];
+	if (fork() == 0)
+		execve((*q)->cmd, args, env);
+	else
+		wait(NULL);
+	free(args);
 	multiple_free("%b", (*q)->args);
 }
 
-int	main(void)
+int	main(int ac, char **av, char **env)
 {
 	t_quotes	*q;
 
+	(void)ac;
+	(void)av;
 	q = (t_quotes *)malloc(sizeof(t_quotes));
 	q->line = readline("minishell$ ");
 	q->line = buildfree(q->line, " ", &ft_strtrim);
@@ -141,7 +134,7 @@ int	main(void)
 	q->index = 0;
 	build_str(0, &q, 0, 0);
 	q->buf[q->count] = '\0';
-	analyse(&q);
+	analyse(&q, env);
 	multiple_free("%a%a%a", q->line, q->buf, q);
 	return (0);
 }
