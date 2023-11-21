2d8147b928cd48785b2475b079e02908b763903b
David Caetano Monteiro <dcaetano@c1r9s10.42porto.com>
2023-11-21 09:09:32 +0000

    a

--- my-lib/main.c
+++ my-lib/main.c
@@ -6,86 +6,113 @@
 /*   By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 19:17:21 by dcaetano          #+#    #+#             */
-/*   Updated: 2023/11/20 22:23:46 by dcaetano         ###   ########.fr       */
+/*   Updated: 2023/11/21 09:09:24 by dcaetano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "my_lib.h"
 #include <readline/readline.h>
 
-void	count_chars(char *line, int *count)
+typedef struct s_quotes
 {
-	int		i;
-	int		status;
-	char	c;
+	int		count;
+	int		index;
+	char	*buf;
+	char	*line;
+}t_quotes;
+
+void	count_chars(t_quotes *q, int status, char c)
+{
+	int	i;
 
 	i = -1;
-	status = 0;
-	c = 0;
-	while (line[++i])
+	while (q->line[++i])
 	{
-		if (status == 0 && (line[i] == '\'' || line[i] == '\"'))
+		if (status == 0 && (q->line[i] == '\'' || q->line[i] == '\"'))
 		{
 			status = 1;
-			c = line[i];
+			c = q->line[i];
 			continue ;
 		}
-		if (status == 1 && line[i] == c)
+		if (status == 1 && q->line[i] == c)
 		{
 			status = 0;
 			c = 0;
 			continue ;
 		}
-		if ((line[i] == ' ' && line[i - 1] != ' ' && status == 0) || \
-			((line[i] != ' ' && status == 0) || status == 1))
-			*count += 1;
+		if ((q->line[i] == ' ' && q->line[i - 1] != ' ' && status == 0) || \
+			((q->line[i] != ' ' && status == 0) || status == 1))
+			q->count++;
 	}
 }
 
-void	build_str(char *line, int j, char *buf)
+char	get_char(char *line, int status, int i)
 {
-	int		i;
-	int		status;
-	char	c;
+	if (((line[i] != ' ' && status == 0) || status == 1))
+		return (line[i]);
+	else if ((line[i] == ' ' && line[i - 1] != ' ' && status == 0))
+		return ('\n');
+	return (0);
+}
 
-	i = -1;
+void	build_str(int j, t_quotes *q, char c)
+{
+	int	i;
+	int	status;
+
 	status = 0;
-	c = 0;
-	while (line[++i])
+	i = -1;
+	while (q->line[++i])
 	{
-		if (status == 0 && (line[i] == '\'' || line[i] == '\"'))
+		if (status == 0 && (q->line[i] == '\'' || q->line[i] == '\"'))
 		{
 			status = 1;
-			c = line[i];
+			c = q->line[i];
 			continue ;
 		}
-		if (status == 1 && line[i] == c)
+		if (status == 1 && q->line[i] == c)
 		{
 			status = 0;
 			c = 0;
 			continue ;
 		}
-		if ((line[i] == ' ' && line[i - 1] != ' ' && status == 0) || \
-			((line[i] != ' ' && status == 0) || status == 1))
-			buf[j++] = line[i];
+		if ((q->line[i] == ' ' && q->line[i - 1] != ' ' && status == 0) || \
+			((q->line[i] != ' ' && status == 0) || status == 1))
+			q->buf[j++] = get_char(q->line, status, i);
 	}
+}
+
+void	analyse(t_quotes *q)
+{
+	char	**args;
+	int		i;
+
+	args = ft_split(q->buf, '\n');
+	i = -1;
+	while (args[++i])
+	{
+		printf("%s", args[i]);
+		if (args[i + 1])
+			printf(" ");
+	}
+	printf("\n");
+	multiple_free("%b", args);
 }
 
 int	main(void)
 {
-	char	*line;
-	char	*buf;
-	int		count;
+	t_quotes	q;
 
-	line = readline("minishell$ ");
-	line = buildfree(line, " ", &ft_strtrim);
-	count = 0;
-	count_chars(line, &count);
-	buf = malloc(sizeof(char *) * (count + 1));
-	build_str(line, 0, buf);
-	free(line);
-	buf[count] = '\0';
-	printf("%s\n", buf);
-	free(buf);
+	q.line = readline("minishell$ ");
+	q.line = buildfree(q.line, " ", &ft_strtrim);
+	q.count = 0;
+	count_chars(&q, 0, 0);
+	q.buf = malloc(sizeof(char *) * (q.count + 1));
+	q.index = 0;
+	build_str(0, &q, 0);
+	free(q.line);
+	q.buf[q.count] = '\0';
+	analyse(&q);
+	free(q.buf);
 	return (0);
 }
