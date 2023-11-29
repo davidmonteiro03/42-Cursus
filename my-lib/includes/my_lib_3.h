7264c20e57571bd5610e41c9b9ebd80d9534350f
David Caetano Monteiro <dcaetano@c1r9s10.42porto.com>
2023-11-29 13:44:54 +0000

    wildcard loading...

--- my-lib/includes/my_lib_3.h
+++ my-lib/includes/my_lib_3.h
@@ -6,7 +6,7 @@
 /*   By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 15:38:33 by dcaetano          #+#    #+#             */
-/*   Updated: 2023/11/29 10:17:12 by dcaetano         ###   ########.fr       */
+/*   Updated: 2023/11/29 13:01:49 by dcaetano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@
 # include <readline/readline.h>
 # include <dirent.h>
 # include <sys/stat.h>
+# include <signal.h>
 
 # define TRUE 1
 # define FALSE 0
@@ -35,11 +36,11 @@ typedef struct s_gb
 	char			**ag;
 }t_gb;
 
+// Exec utils
+void	wild_get(t_gb *gb);
+
 // Free
 char	*buildfree(char *s1, char *s2, char *(*f)(const char *, const char *));
 void	multiple_free(const char *format, ...);
-
-// Utils
-void	send_to_trash(t_gb *gb);
 
 #endif
--- my-lib/main_3.c
+++ my-lib/main_3.c
@@ -6,91 +6,81 @@
 /*   By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 07:17:48 by dcaetano          #+#    #+#             */
-/*   Updated: 2023/11/29 11:02:31 by dcaetano         ###   ########.fr       */
+/*   Updated: 2023/11/29 13:44:48 by dcaetano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/my_lib_3.h"
 
-int	wild_match(char *path, char *ptrn)
+void	new_prompt(int sig)
 {
-	if (!*path && !*ptrn)
-		return (TRUE);
-	if (*path == '*' && *(path + 1) && !*ptrn)
-		return (FALSE);
-	if (*path == '?' || *path == *ptrn)
-		return (wild_match(path + 1, ptrn + 1));
-	if (*path == '*')
-		return (wild_match(path + 1, ptrn) || wild_match(path, ptrn + 1));
-	return (FALSE);
-}
+	(void)sig;
 
-void	chk(t_gb *gb, int mode, char *str)
-{
-	if (mode)
-		gb->ag[gb->ai++] = ft_strdup(str);
-	else
-		gb->ai++;
-}
-
-void	wild_prep(t_gb *gb, char **as, int mode, int i)
-{
-	while (as[++i])
+	if (sig == SIGINT)
 	{
-		gb->dr = opendir(".");
-		if (!gb->dr)
-			return ;
-		if (ft_strchr(as[i], '*') || ft_strchr(as[i], '?'))
-		{
-			while (1)
-			{
-				gb->pt = readdir(gb->dr);
-				if (!gb->pt)
-					break ;
-				if (gb->pt->d_name[0] != '.' && \
-					!stat(gb->pt->d_name, &gb->st) && \
-					wild_match(as[i], gb->pt->d_name))
-					chk(gb, mode, gb->pt->d_name);
-			}
-		}
-		else
-			chk(gb, mode, as[i]);
-		closedir(gb->dr);
+		rl_replace_line("", 0);
+		rl_on_new_line();
+		printf("\n");
+		rl_redisplay();
 	}
 }
 
-void	wild_get(t_gb *gb)
+int	exec(t_gb *gb, char **e)
 {
+	gb->ln = readline("wildcard $ ");
+	if (!gb->ln)
+		return (0);
+	if (!*gb->ln)
+		return (free(gb->ln), 0);
+	gb->as = ft_split(gb->ln, ' ');
+	if (!gb->as)
+		return (free(gb->ln), 1);
+	if (!*gb->as)
+		return (free(gb->ln), free(gb->as), 1);
+	gb->cm = buildfree(ft_strdup("/bin/"), ft_strdup(gb->as[0]), &ft_strjoin);
+	if (!gb->cm)
+		return (multiple_free("%b%a", gb->as, gb->ln), 1);
+	if (!ft_strncmp(gb->cm, "exit\0", 5))
+		return (multiple_free("%b%a%a", gb->as, gb->ln, gb->cm), 0);
+	wild_get(gb);
+	if (!gb->ag)
+		return (multiple_free("%a%a%b", gb->ln, gb->cm, gb->as), 1);
+	if (!*gb->ag)
+		return (multiple_free("%a%a%b%a", gb->ln, gb->cm, gb->as, gb->ag), 1);
+	if (!fork())
+		execve(gb->cm, gb->ag, e);
+	else
+		wait(NULL);
+	return (multiple_free("%a%a%b%b", gb->ln, gb->cm, gb->as, gb->ag), 1);
+}
+
+t_gb	*init_gb(void)
+{
+	t_gb	*gb;
+
+	gb = (t_gb *)malloc(sizeof(t_gb));
+	if (!gb)
+		return (NULL);
+	gb->ln = NULL;
+	gb->dr = NULL;
+	gb->pt = NULL;
+	gb->cm = NULL;
+	gb->as = NULL;
 	gb->ai = 0;
-	wild_prep(gb, gb->as, 0, -1);
-	gb->ag = (char **)malloc(sizeof(char *) * (gb->ai + 1));
-	gb->ag[gb->ai] = NULL;
-	gb->ai = 0;
-	wild_prep(gb, gb->as, 1, -1);
+	gb->ag = NULL;
+	return (gb);
 }
 
 int	main(int ac, char **av, char **env)
 {
 	t_gb	*gb;
 
-	gb = (t_gb *)malloc(sizeof(t_gb));
+	gb = init_gb();
 	if (!gb)
 		return (0);
-	gb->ln = readline("wildcard $ ");
-	if (!gb->ln)
-		return (send_to_trash(gb), 0);
-	if (!*gb->ln)
-		return (send_to_trash(gb), 0);
-	gb->as = ft_split(gb->ln, ' ');
-	if (!gb->as)
-		return (send_to_trash(gb), 0);
-	gb->cm = buildfree(ft_strdup("/bin/"), ft_strdup(gb->as[0]), &ft_strjoin);
-	if (!gb->cm)
-		return (send_to_trash(gb), 0);
-	wild_get(gb);
-	if (!fork())
-		execve(gb->cm, gb->ag, env);
-	else
-		wait(NULL);
-	return ((void)ac, (void)av, send_to_trash(gb), 0);
+	signal(SIGINT, new_prompt);
+	signal(SIGQUIT, SIG_IGN);
+	while (exec(gb, env))
+		;
+	return ((void)ac, (void)av, free(gb), 0);
 }
