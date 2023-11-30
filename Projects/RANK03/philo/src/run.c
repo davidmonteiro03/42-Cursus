dfbd97cb44180b6ff145b602978bd5896dbfb323
David Caetano Monteiro <dcaetano@c1r9s10.42porto.com>
2023-11-30 18:03:25 +0000

    a

--- Projects/RANK03/philo/src/run.c
+++ Projects/RANK03/philo/src/run.c
@@ -6,7 +6,7 @@
 /*   By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 15:01:04 by dcaetano          #+#    #+#             */
-/*   Updated: 2023/11/28 17:33:15 by dcaetano         ###   ########.fr       */
+/*   Updated: 2023/11/30 17:28:02 by dcaetano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ void	*ph_th(void *ag)
 	t_ph	*ph;
 
 	ph = (t_ph *)ag;
-	printf("philo %d\n", ph->id);
+	printf("philo %d", ph->id);
+	printf(" => timestamp %ld\n", ph_gtm());
 	return (NULL);
 }
 
