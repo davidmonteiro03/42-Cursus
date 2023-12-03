ba5b5ad396b3a407d1fd143e3af819ae955f4c8e
David Caetano Monteiro <dcaetano@c1r9s10.42porto.com>
2023-12-03 17:55:27 +0000

    a

--- Projects/RANK03/philo/src/display.c
+++ Projects/RANK03/philo/src/display.c
@@ -6,7 +6,7 @@
 /*   By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 20:17:56 by dcaetano          #+#    #+#             */
-/*   Updated: 2023/12/03 15:31:14 by dcaetano         ###   ########.fr       */
+/*   Updated: 2023/12/03 17:52:09 by dcaetano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,9 @@
 
 void	ph_status(t_philo *philo, char *status)
 {
-	printf("%5lld ms | %3d %-20s | NM: %d\n", \
+	printf("%5lld ms | %3d %-20s\n", \
 		ph_get_time() - philo->data->start, \
 		philo->id, \
-		status, \
-		philo->num_of_meals \
+		status \
 	);
 }
