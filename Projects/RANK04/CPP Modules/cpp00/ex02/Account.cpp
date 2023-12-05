4042b53255c4ac6e23ebb3ef06f78bc29062ad32
David Caetano Monteiro <dcaetano@c1r9s10.42porto.com>
2023-12-05 10:38:20 +0000

    a

---
+++ Projects/RANK04/CPP Modules/cpp00/ex02/Account.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/05 08:35:28 by dcaetano          #+#    #+#             */
+/*   Updated: 2023/12/05 10:37:16 by dcaetano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+#include <iostream>
+#include <iomanip>
+
+int	Account::_nbAccounts = 0;
+
+int	Account::_totalAmount = 0;
+
+int	Account::_totalNbDeposits = 0;
+
+int	Account::_totalNbWithdrawals = 0;
+
+Account::Account(int initial_deposit)
+{
+}
+
+void	Account::_displayTimestamp(void)
+{
+}
