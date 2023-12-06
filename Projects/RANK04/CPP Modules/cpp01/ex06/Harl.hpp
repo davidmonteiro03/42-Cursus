968bfae160e932535d813c3a89adeeaac174b44f
David Caetano Monteiro <dcaetano@c1r9s10.42porto.com>
2023-12-06 10:13:04 +0000

    CPP01 done

--- Projects/RANK04/CPP Modules/cpp01/ex06/Harl.hpp
+++ Projects/RANK04/CPP Modules/cpp01/ex06/Harl.hpp
@@ -1,32 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/06 08:48:02 by dcaetano          #+#    #+#             */
-/*   Updated: 2023/12/06 09:30:42 by dcaetano         ###   ########.fr       */
+/*   Created: 2023/12/06 08:49:41 by dcaetano          #+#    #+#             */
+/*   Updated: 2023/12/06 09:01:20 by dcaetano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Harl.hpp"
+#ifndef HARL_HPP
+# define HARL_HPP
 
-int	main(void)
+# include <iostream>
+
+class Harl
 {
-	std::string	level;
-	Harl	Harl;
+	private:
+		void	debug(void);
+		void	info(void);
+		void	warning(void);
+		void	error(void);
+	public:
+		Harl(void);
+		~Harl(void);
+		void	complain(std::string level);
+};
 
-	while (1)
-	{
-		std::cout << "Enter a level: ";
-		std::getline(std::cin, level);
-		if (std::cin.eof())
-		{
-			std::cout << std::endl;
-			break ;
-		}
-		Harl.complain(level);
-	}
-	return (0);
-}
+typedef void (Harl::*t_func)(void);
+
+#endif
--- Projects/RANK04/CPP Modules/cpp01/ex06/main.cpp
+++ Projects/RANK04/CPP Modules/cpp01/ex06/main.cpp
@@ -6,27 +6,21 @@
 /*   By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 08:48:02 by dcaetano          #+#    #+#             */
-/*   Updated: 2023/12/06 09:30:42 by dcaetano         ###   ########.fr       */
+/*   Updated: 2023/12/06 09:38:02 by dcaetano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
-int	main(void)
+int	main(int argc, char **argv)
 {
-	std::string	level;
 	Harl	Harl;
 
-	while (1)
+	if (argc != 2)
 	{
-		std::cout << "Enter a level: ";
-		std::getline(std::cin, level);
-		if (std::cin.eof())
-		{
-			std::cout << std::endl;
-			break ;
-		}
-		Harl.complain(level);
+		std::cout << "Wrong number of arguments" << std::endl;
+		return (1);
 	}
+	Harl.complain(argv[1]);
 	return (0);
 }
