6083737076f2905f86d6df77348dc5c1f168bfe9
David Caetano Monteiro <dcaetano@c1r9s10.42porto.com>
2023-12-08 16:30:07 +0000

    a

---
+++ Projects/RANK04/cub3d/include/cub3d.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/08 16:28:50 by dcaetano          #+#    #+#             */
+/*   Updated: 2023/12/08 16:29:29 by dcaetano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include "../libft/libft.h"
+# include "../libft/get_next_line.h"
+# include "../minilibx-linux/mlx.h"
+# include <X11/X.h>
+# include <X11/keysym.h>
+
+#endif
