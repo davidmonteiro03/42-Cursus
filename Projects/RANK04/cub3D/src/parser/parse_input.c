f36687c0df30f1bcdfa537174916b32203c190c3
David Caetano Monteiro <dcaetano@c1r9s10.42porto.com>
2023-12-08 18:14:04 +0000

    a

--- Projects/RANK04/cub3D/src/parser/parse_input.c
+++ Projects/RANK04/cub3D/src/parser/parse_input.c
@@ -1,20 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   parse_input.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/08 16:25:18 by dcaetano          #+#    #+#             */
-/*   Updated: 2023/12/08 18:11:30 by dcaetano         ###   ########.fr       */
+/*   Created: 2023/12/08 18:07:57 by dcaetano          #+#    #+#             */
+/*   Updated: 2023/12/08 18:13:33 by dcaetano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "include/cub3D.h"
+#include "../../include/cub3D.h"
 
-int	main(int argc, char **argv)
+bool	cub_check_input(int argc, char **argv)
 {
-	if (!cub_check_input(argc, argv))
-		return (1);
-	return (0);
+	(void)argv;
+	if (argc != 2)
+		return (false);
+	return (true);
 }
