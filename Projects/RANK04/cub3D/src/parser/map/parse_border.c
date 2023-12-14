ff98d0a4ec7d281a9916addf95c9c76db88f4ffc
davidmonteiro03 <damonteiro12446@gmail.com>
2023-12-14 17:40:14 +0000

    working in progress...

--- Projects/RANK04/cub3D/src/parser/map/parse_border.c
+++ Projects/RANK04/cub3D/src/parser/map/parse_border.c
@@ -6,40 +6,76 @@
 /*   By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 11:52:33 by dcaetano          #+#    #+#             */
-/*   Updated: 2023/12/14 17:03:23 by dcaetano         ###   ########.fr       */
+/*   Updated: 2023/12/14 17:39:45 by dcaetano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../include/cub3D.h"
 
+void	tmp_simple_display(char **strs, int i, int j)
+{
+	write(1, "=========== IGNORE ========= ", 29);
+	while (strs[i][++j])
+	{
+		if (strs[i][j] == ' ')
+			write(1, "-", 1);
+		else
+			write(1, &strs[i][j], 1);
+	}
+	write(1, "\n", 1);
+}
+
+void	display_before(char **strs, int i)
+{
+	int	k;
+
+	write(1, "\n---- BEFORE ------\n", 20);
+	k = -1;
+	while (++k < i)
+	{
+		write(1, strs[k], ft_strlen(strs[k]));
+		write(1, "\n", 1);
+	}
+	write(1, "---- CURRENT ------\n", 20);
+}
+
+void	display_after(char **strs, int i)
+{
+	int	k;
+
+	write(1, "\n----  AFTER  ------\n", 21);
+	k = i;
+	while (++k < cub_strs_size(strs))
+	{
+		write(1, strs[k], ft_strlen(strs[k]));
+		write(1, "\n", 1);
+	}
+	write(1, "---- CURRENT ------\n", 20);
+}
+
+void	tmp_ultimate_display(char **strs, int i, int j)
+{
+	write(1, "========== CONSIDER ======== ", 30);
+	display_before(strs, i);
+	while (strs[i][++j])
+	{
+		if (strs[i][j] == ' ')
+			write(1, "-", 1);
+		else
+			write(1, &strs[i][j], 1);
+	}
+	display_after(strs, i);
+	write(1, "\n", 1);
+}
+
 void	cub_check_walls(char **strs, int i)
 {
-	int	j;
-
 	while (strs[++i])
 	{
 		if (i == 0 || i == cub_strs_size(strs) - 1)
-		{
-			j = -1;
-			while (strs[i][++j])
-			{
-				if (strs[i][j] == ' ')
-					write(1, "-", 1);
-				else
-					write(1, &strs[i][j], 1);
-			}
-			write(1, "\n", 1);
-			continue ;
-		}
-		j = -1;
-		while (strs[i][++j])
-		{
-			if (strs[i][j] == ' ')
-				write(1, "-", 1);
-			else
-				write(1, &strs[i][j], 1);
-		}
-		write(1, "\n", 1);
+			tmp_simple_display(strs, i, -1);
+		else
+			tmp_ultimate_display(strs, i, -1);
 	}
 }
 
