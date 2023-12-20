b169292344e871b2aa6f1d7e22c2c1fff7615d6f
David Caetano Monteiro <dcaetano@c1r9s10.42porto.com>
2023-12-20 21:59:45 +0000

    a

---
+++ my-lib/includes/my_lib_7.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   my_lib_7.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/16 11:35:47 by dcaetano          #+#    #+#             */
+/*   Updated: 2023/12/20 17:24:43 by dcaetano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MY_LIB_7_H
+# define MY_LIB_7_H
+
+# include "../libft/libft.h"
+# include "../libft/get_next_line.h"
+
+// free
+void	multiple_free(const char *format, ...);
+
+#endif
