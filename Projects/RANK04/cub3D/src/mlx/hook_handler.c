42c43d3e0fdcd17bbcf6314f2e685bf7e21f7376
davidmonteiro03 <damonteiro12446@gmail.com>
2023-12-23 01:05:23 +0000

    minimap almost finished, I think

--- Projects/RANK04/cub3D/src/mlx/hook_handler.c
+++ Projects/RANK04/cub3D/src/mlx/hook_handler.c
@@ -6,7 +6,7 @@
 /*   By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 15:39:40 by dcaetano          #+#    #+#             */
-/*   Updated: 2023/12/23 00:58:22 by dcaetano         ###   ########.fr       */
+/*   Updated: 2023/12/23 01:05:18 by dcaetano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,15 +44,15 @@ int	cub_press_key(int keycode, t_cub *cub)
 {
 	if (keycode == XK_Right)
 		cub->keys.right = true;
-	if (keycode == XK_Left)
+	else if (keycode == XK_Left)
 		cub->keys.left = true;
-	if (keycode == XK_w)
+	else if (keycode == XK_w)
 		cub->keys.w = true;
-	if (keycode == XK_s)
+	else if (keycode == XK_s)
 		cub->keys.s = true;
-	if (keycode == XK_a)
+	else if (keycode == XK_a)
 		cub->keys.a = true;
-	if (keycode == XK_d)
+	else if (keycode == XK_d)
 		cub->keys.d = true;
 	else if (keycode == XK_Escape)
 		cub_exit(cub);
@@ -86,23 +86,6 @@ int	cub_mouse_handler(int keycode, t_cub *cub)
 
 int	cub_render(t_cub *cub)
 {
-	if (cub->keys.left)
-		cub_update_angle(cub, -1);
-	else if (cub->keys.right)
-		cub_update_angle(cub, 1);
-	if (cub->keys.w)
-		move_player(cub, cos(cub->player.angle * M_PI / 180) * \
-			STEP, sin(cub->player.angle * M_PI / 180) * STEP);
-	else if (cub->keys.s)
-		move_player(cub, -cos(cub->player.angle * M_PI / 180) * \
-			STEP, -sin(cub->player.angle * M_PI / 180) * STEP);
-	else if (cub->keys.a)
-		move_player(cub, -cos((cub->player.angle + 90) * \
-			M_PI / 180) * STEP, -sin((cub->player.angle + 90) * \
-			M_PI / 180) * STEP);
-	else if (cub->keys.d)
-		move_player(cub, cos((cub->player.angle + 90) * M_PI / 180) * \
-			STEP, sin((cub->player.angle + 90) * M_PI / 180) * STEP);
 	auto int x, y;
 	mlx_mouse_get_pos(cub->mlx.mlx, cub->mlx.win, &x, &y);
 	printf("%d %d\n", x, y);
