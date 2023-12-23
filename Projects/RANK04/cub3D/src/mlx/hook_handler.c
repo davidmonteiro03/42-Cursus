516ff520c09f4acc289062fbf5f76ead8e50d73d
davidmonteiro03 <damonteiro12446@gmail.com>
2023-12-23 00:55:07 +0000

    minimap almost finished, I think

--- Projects/RANK04/cub3D/src/mlx/hook_handler.c
+++ Projects/RANK04/cub3D/src/mlx/hook_handler.c
@@ -6,7 +6,7 @@
 /*   By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 15:39:40 by dcaetano          #+#    #+#             */
-/*   Updated: 2023/12/23 00:52:35 by dcaetano         ###   ########.fr       */
+/*   Updated: 2023/12/23 00:55:03 by dcaetano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -106,5 +106,8 @@ int	cub_render(t_cub *cub)
 	auto int x, y;
 	mlx_mouse_get_pos(cub->mlx.mlx, cub->mlx.win, &x, &y);
 	printf("%d %d\n", x, y);
-	return (0);
+	mlx_mouse_move(cub->mlx.mlx, cub->mlx.win, \
+		cub->map.width * MMAP_SZ / 2, \
+		cub->map.height * MMAP_SZ / 2);
+	return (usleep(10000), 0);
 }
