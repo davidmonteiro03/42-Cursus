9c56270c01716fd21f7baad440d394ba5820046b
David Caetano Monteiro <dcaetano@c1r9s10.42porto.com>
2024-02-04 18:00:24 +0000

    git updates

--- git_script.sh
+++ git_script.sh
@@ -1,5 +1,11 @@
 clear
+echo "=========== GIT STATUS ==========="
 git status
+echo "===========   GIT ADD  ==========="
 git add .
+echo "=========== GIT STATUS ==========="
+git status
+echo "=========== GIT COMMIT ==========="
 git commit -m "git updates"
+echo "============ GIT PUSH ============"
 git push
