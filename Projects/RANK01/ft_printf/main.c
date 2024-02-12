5477b6a9ddf69a7f3309765f88bc75e2edae4881
davidmonteiro03 <damonteiro12446@gmail.com>
2024-02-12 17:31:59 +0000

    bonus of printf are wrong :(

--- Projects/RANK01/ft_printf/main.c
+++ Projects/RANK01/ft_printf/main.c
@@ -1,21 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_findbug_bonus.c                                 :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/09 22:21:18 by dcaetano          #+#    #+#             */
-/*   Updated: 2024/02/09 22:21:22 by dcaetano         ###   ########.fr       */
+/*   Created: 2024/02/12 17:16:50 by dcaetano          #+#    #+#             */
+/*   Updated: 2024/02/12 17:30:38 by dcaetano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	ft_findbug_bonus(t_list **arg, char c)
+int	main(void)
 {
-	if (ft_tolower(c) == 'x')
-		ft_checkhex_bonus(arg);
-	if (ft_strchr("di", c))
-		ft_checkint_bonus(arg);
+	printf("%.5s", "ola mundo");
+	printf("$\n");
+	fflush(stdout);
+	ft_printf("%.5s", "ola mundo");
+	printf("$\n");
+	return (0);
 }
