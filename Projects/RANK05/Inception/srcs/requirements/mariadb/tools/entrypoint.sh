04f17ce0dc53f503f39a121b4f9baeb45cf2ae7d
David Monteiro <damonteiro12446@gmail.com>
2024-03-22 11:26:00 +0000

    php is working!!!!!!!!!!!!!!! :) but wp gives an db error... :(

--- Projects/RANK05/Inception/srcs/requirements/mariadb/tools/entrypoint.sh
+++ Projects/RANK05/Inception/srcs/requirements/mariadb/tools/entrypoint.sh
@@ -6,8 +6,11 @@
 #    By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/03/21 18:50:41 by dcaetano          #+#    #+#              #
-#    Updated: 2024/03/21 18:50:51 by dcaetano         ###   ########.fr        #
+#    Updated: 2024/03/22 11:20:41 by dcaetano         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/sh
+
+# create database
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
