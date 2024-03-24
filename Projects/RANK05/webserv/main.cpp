43bb19aa25c8f03a9e2b66fc04c12cfed0a08f61
David Monteiro <damonteiro12446@gmail.com>
2024-03-24 13:44:17 +0000

    git updates

---
+++ Projects/RANK05/webserv/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/24 12:12:20 by dcaetano          #+#    #+#             */
+/*   Updated: 2024/03/24 12:44:23 by dcaetano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <vector>
+
+struct Server
+{
+	std::string name;
+	std::vector<Server> children;
+};
+
+int main(void)
+{
+	Server server;
+	server.name = "server";
+	return 0;
+}
