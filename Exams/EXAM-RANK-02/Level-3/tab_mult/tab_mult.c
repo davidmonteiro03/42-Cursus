69c6856757f0658e65bc31a56e6fad43653ece05
David Monteiro <damonteiro12446@gmail.com>
2024-04-03 12:22:30 +0100

    git updates

---
+++ Exams/EXAM-RANK-02/Level-3/tab_mult/tab_mult.c
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tab_mult.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/03 12:00:22 by dcaetano          #+#    #+#             */
+/*   Updated: 2024/04/03 12:10:52 by dcaetano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_atoi(const char *str)
+{
+	int	ret;
+	int	sign;
+
+	ret = 0;
+	sign = 1;
+	while (*str && *str == ' ')
+		str++;
+	if (*str == '-' || *str == '+')
+		if (*str++ == '-')
+			sign = -1;
+	while (*str && *str >= '0' && *str <= '9')
+		ret = ret * 10 + (*str++ - '0');
+	return (ret * sign);
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	if (str == 0)
+		return ;
+	while (*str)
+		ft_putchar(*str++);
+}
+
+void	ft_putnbr(int nbr)
+{
+	if (nbr == -2147483648)
+		return (ft_putstr("-2147483648"));
+	if (nbr < 0)
+	{
+		ft_putchar('-');
+		nbr = -nbr;
+	}
+	if (nbr > 9)
+	{
+		ft_putnbr(nbr / 10);
+		ft_putnbr(nbr % 10);
+	}
+	else
+		ft_putchar(nbr % 10 + '0');
+}
+
+void	tab_mult(int num)
+{
+	int	i;
+
+	i = 0;
+	while (i++ < 9)
+	{
+		if (i > 1)
+			ft_putchar('\n');
+		ft_putnbr(i);
+		ft_putstr(" x ");
+		ft_putnbr(num);
+		ft_putstr(" = ");
+		ft_putnbr(i * num);
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+		tab_mult(ft_atoi(argv[1]));
+	ft_putchar('\n');
+	return (0);
+}
