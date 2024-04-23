d4d8f6ab403ad832dbd928de1319c3cad11098ce
David Monteiro <damonteiro12446@gmail.com>
2024-04-23 09:58:29 +0100

    git updates

--- Projects/RANK01/ft_printf/ft_printf.h
+++ Projects/RANK01/ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 08:05:05 by dcaetano          #+#    #+#             */
-/*   Updated: 2024/04/23 09:29:06 by dcaetano         ###   ########.fr       */
+/*   Updated: 2024/04/23 09:57:36 by dcaetano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ int		ft_printf(const char *format, ...); // ft_printf
 // ----- format functions ---- //
 void	ft_putchar(const char c, int *ret);
 void	ft_putstr(const char *str, int *ret);
+void	ft_putptr(const void *ptr, int *ret);
 
 // ----- utils functions ----- //
 void	ft_format(const char *format, va_list args, int *ret);
