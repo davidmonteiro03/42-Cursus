2055dedc14ea414cfb0c28b63b7aabda5ce36f36
davidmonteiro03 <damonteiro12446@gmail.com>
2024-08-23 08:31:11 +0100

    git updates

--- Exams/EXAM-RANK-03/ft_printf/main.c
+++ Exams/EXAM-RANK-03/ft_printf/main.c
@@ -6,7 +6,7 @@
 /*   By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 19:18:19 by dcaetano          #+#    #+#             */
-/*   Updated: 2023/11/09 19:19:38 by dcaetano         ###   ########.fr       */
+/*   Updated: 2024/08/23 08:30:27 by dcaetano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,28 @@
 #include <stdio.h>
 
 // choose your arguments
+static void	execute(int (*print)(const char *, ...))
+{
+	int	ret;
+
+	if (print == NULL || (print != printf && print != ft_printf))
+		return ;
+	if (print == printf)
+		printf("=== PRINTF ===\n");
+	else
+		printf("=== FT_PRINTF ===\n");
+	printf("--- output ---\n");
+	ret = print("%s %d %x %d %s\n", (char *) NULL, 42, 42, 100, "Hello World!");
+	printf("--- return ---\n");
+	printf("ret = %d\n", ret);
+	printf("\n");
+	if (print == printf)
+		fflush(stdout);
+}
+
 int	main(void)
 {
-	printf("%d\n", \
-		ft_printf("%s %d %x %d %s\n", NULL, 42, 42, 100, "Hello World!") \
-	);
+	execute(printf);
+	execute(ft_printf);
 	return (0);
 }
