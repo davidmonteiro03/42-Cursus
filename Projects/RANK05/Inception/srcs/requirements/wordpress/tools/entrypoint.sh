5423f2f5718de3b3955619542dc2aac5bd018d55
davidmonteiro03 <damonteiro12446@gmail.com>
2024-09-19 13:00:08 +0100

    git updates

--- Projects/RANK05/Inception/srcs/requirements/wordpress/tools/entrypoint.sh
+++ Projects/RANK05/Inception/srcs/requirements/wordpress/tools/entrypoint.sh
@@ -6,70 +6,70 @@
 #    By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/03/21 18:38:51 by dcaetano          #+#    #+#              #
-#    Updated: 2024/09/19 12:54:25 by dcaetano         ###   ########.fr        #
+#    Updated: 2024/09/19 12:59:52 by dcaetano         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/sh
 
-# Definir variáveis
+# Define variables
 WORDPRESS_URL="https://wordpress.org/latest.tar.gz"
 WP_CLI_URL="https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar"
 WORDPRESS_DIR="/var/www/html"
 PHP_FPM_CONF="/etc/php/7.4/fpm/pool.d/www.conf"
 TMP_CONF="/tmp/www.conf"
 
-# Baixar e extrair WordPress
-echo "Baixando WordPress..."
+# Download and extract WordPress
+echo "Downloadind WordPress..."
 wget $WORDPRESS_URL -O latest.tar.gz
 if [ $? -ne 0 ]; then
-    echo "Erro ao baixar WordPress"
+    echo "Error downloading WordPress"
     exit 1
 fi
 
-echo "Extraindo WordPress..."
+echo "Extracting WordPress..."
 tar -xvf latest.tar.gz
 rm -rf latest.tar.gz
 
-# Mover arquivos do WordPress
-echo "Movendo arquivos do WordPress..."
+# Move WordPress files
+echo "Moving WordPress files..."
 if [ -d "wordpress" ]; then
     mv wordpress/* $WORDPRESS_DIR
 else
-    echo "Diretório wordpress não encontrado"
+    echo "Wordpress directory not found"
     exit 1
 fi
 
-# Configurar permissões
-echo "Configurando permissões..."
+# Setup permissions
+echo "Setting up permitions..."
 mkdir -p $WORDPRESS_DIR
 chown -R www-data:www-data $WORDPRESS_DIR
 chmod -R 777 $WORDPRESS_DIR
 
-# Configurar PHP-FPM
-echo "Configurando PHP-FPM..."
+# Setup PHP-FPM
+echo "Setting up PHP-FPM..."
 mkdir -p /run/php
 chown -R www-data:www-data $WORDPRESS_DIR
 if [ -f $TMP_CONF ]; then
     mv $TMP_CONF $PHP_FPM_CONF
 else
-    echo "Arquivo de configuração PHP-FPM não encontrado"
+    echo "PHP-FPM configuration file not found"
     exit 1
 fi
 
-# Baixar e configurar WP-CLI
-echo "Baixando WP-CLI..."
+# Download e setup WP-CLI
+echo "Downloading WP-CLI..."
 wget $WP_CLI_URL -O wp-cli.phar
 if [ $? -ne 0 ]; then
-    echo "Erro ao baixar WP-CLI"
+    echo "Error downloading WP-CLI"
     exit 1
 fi
 
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
-# Configurar WordPress
-echo "Configurando WordPress..."
+# Setup WordPress
+echo "Setting up WordPress..."
 cd $WORDPRESS_DIR
 cp wp-config-sample.php wp-config.php
 sed -i "s/database_name_here/$DB_NAME/g" wp-config.php
@@ -77,8 +77,8 @@ sed -i "s/username_here/$DB_USER/g" wp-config.php
 sed -i "s/password_here/$DB_PASS/g" wp-config.php
 sed -i "s/localhost/$DB_HOST/g" wp-config.php
 
-# Instalar WordPress
-echo "Instalando WordPress..."
+# Install WordPress
+echo "Installing WordPress..."
 wp core install --url="https://$DOMAIN" \
     --title="$WP_TITLE" \
     --admin_user="$DB_USER" \
@@ -88,14 +88,14 @@ wp core install --url="https://$DOMAIN" \
     --path="$WORDPRESS_DIR" \
     --allow-root
 
-# Criar usuário WordPress
-echo "Criando usuário WordPress..."
+# Create WordPress user
+echo "Creating WordPress user..."
 wp user create $WP_USER $WP_MAIL \
     --role=author \
     --user_pass="$WP_PASS" \
     --path="$WORDPRESS_DIR" \
     --allow-root
 
-# Iniciar PHP-FPM
-echo "Iniciando PHP-FPM..."
+# Start PHP-FPM
+echo "Starting PHP-FPM..."
 /usr/sbin/php-fpm7.4 -F
