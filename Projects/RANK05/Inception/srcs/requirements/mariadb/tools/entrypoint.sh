0d9b8d3d2e4161940817d61685193e8363298e33
davidmonteiro03 <damonteiro12446@gmail.com>
2024-09-19 13:12:31 +0100

    git updates

--- Projects/RANK05/Inception/srcs/requirements/mariadb/tools/entrypoint.sh
+++ Projects/RANK05/Inception/srcs/requirements/mariadb/tools/entrypoint.sh
@@ -5,19 +5,15 @@
 #                                                     +:+ +:+         +:+      #
 #    By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2024/03/22 12:59:19 by dcaetano          #+#    #+#              #
-#    Updated: 2024/03/22 15:55:13 by dcaetano         ###   ########.fr        #
+#    Created: 2024/09/19 13:11:43 by dcaetano          #+#    #+#              #
+#    Updated: 2024/09/19 13:12:05 by dcaetano         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/sh
 
-run_command() {
-    eval "$1"
-}
-
 echo "Starting MariaDB installation..."
-run_command "service mariadb start"
+service mariadb start
 
 config="
 [client-server]
@@ -33,42 +29,47 @@ socket = /run/mysqld/mysqld.sock
 port = 3306
 bind-address = 0.0.0.0
 "
-echo "$config" > /etc/mysql/my.cnf
+
+if [ -w /etc/mysql/my.cnf ]; then
+    echo "$config" > /etc/mysql/my.cnf
+else
+    echo "Warning: /etc/mysql/my.cnf cannot be written."
+fi
 
 if [ ! -d "/var/lib/mysql/$DB_NAME" ]; then
-    run_command "
     mysql_secure_installation << EOF
-    $DB_ROOT
-    y
-    n
-    y
-    y
-    y
-    y
-    EOF
-    "
+$DB_ROOT
+y
+n
+y
+y
+y
+y
+EOF
     echo "MySQL secure installation completed."
 fi
 
 echo "MariaDB Starting Setup."
 
-run_command "
 mariadb <<EOF
 CREATE DATABASE IF NOT EXISTS $DB_NAME;
-CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';
-CREATE USER '$WP_USER'@'%' IDENTIFIED BY '$WP_PASS';
+CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';
+CREATE USER IF NOT EXISTS '$WP_USER'@'%' IDENTIFIED BY '$WP_PASS';
 GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
 GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$WP_USER'@'%';
 SELECT user, host FROM mysql.user;
 FLUSH PRIVILEGES;
 exit
 EOF
-"
 
 echo "MariaDB setup completed."
 
 echo "Stopping MariaDB..."
-run_command "service mariadb stop"
+if pgrep mariadbd > /dev/null; then
+    service mariadb stop
+else
+    echo "MariaDB is not running."
+fi
 
 echo "Starting MariaDB as a Server..."
-run_command "exec mysqld --datadir=/var/lib/mysql"
+exec mysqld --datadir=/var/lib/mysql
